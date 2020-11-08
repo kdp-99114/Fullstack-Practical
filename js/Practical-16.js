5b3e4739b791195f835d0e325763c6292adc501a
kdp-99114 <kardam.pandey_cs18@gla.ac.in>
2020-11-08 14:49:34 +0530

    add event and add function createList

--- js/Practical-16.js
+++ js/Practical-16.js
@@ -6,4 +6,33 @@ var todoList = document.querySelector(".todo-list");
 
 // Event Handlers
 
+todoButton.onclick = createList;
+
 // Functions
+
+function createList(e) {
+    e.preventDefault();
+
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("todo");
+
+    var newLi = document.createElement("li");
+    newLi.classList.add("todo-item");
+    newLi.innerHTML = "List";
+
+    newDiv.appendChild(newLi);
+
+    var checkbtn = document.createElement("button");
+    checkbtn.classList.add("check-btn");
+    checkbtn.innerHTML = '<li class="fa fa-check"></li>';
+
+    newDiv.appendChild(checkbtn);
+
+    var deletebtn = document.createElement("button");
+    deletebtn.classList.add("delete-btn");
+    deletebtn.innerHTML = '<li class="fa fa-trash"></li>';
+
+    newDiv.appendChild(deletebtn);
+
+    todoList.appendChild(newDiv);
+}
