ba657eaa642ba197abe944107704a9d96e6f586e
kdp-99114 <kardam.pandey_cs18@gla.ac.in>
2020-11-08 14:28:49 +0530

    add selectors in script

---
+++ js/Practical-16.js
@@ -0,0 +1,9 @@
+// selectors
+
+var todoInput = document.querySelector(".todo-input");
+var todoButton = document.querySelector(".todo-button");
+var todoList = document.querySelector(".todo-list");
+
+// Event Handlers
+
+// Functions
